af49e08ded3968327854cef2f155fb3fbb01d1d9
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-03 16:26:35 +0300

    added a customrgistation function.

--- clone/views.py
+++ clone/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import customRegistrationForm
 
 # Create your views here.
+def customUserRegister(request):
+    form = customRegistrationForm
+    if request.method == 'POST':
+        form = customRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponse('Successfully registered')
+    return render(request, 'auth/register.html',{'form': form})
