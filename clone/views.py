47aaf2ca36583e95d4562c88bfc1538730a2b37e
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-04 17:41:46 +0300

    created a login function.

--- clone/views.py
+++ clone/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import customRegistrationForm
+from .forms import customRegistrationForm, LoginForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .emails import send_welcome_email
 
 # Create your views here.
@@ -16,5 +17,25 @@ def customUserRegister(request):
             recipient = User(first_name=first_name, email=email)
             send_welcome_email(first_name,email)
             form.save()
-        return HttpResponse('Successfully registered')
+        return redirect(login_user)
     return render(request, 'auth/register.html',{'form': form})
+
+
+def login_user(request):
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            usern=form.cleaned_data['email']
+            passw=form.cleaned_data['password']
+            user=authenticate(request,username=usern,password=passw)
+            if user is not None:
+                login(request,user)
+                return HttpResponse('Login successful')
+            else:
+                return HttpResponse('Such a user does not exist')
+        else:
+            return HttpResponse("Form is not Valid")
+    
+    return render(request,'auth/login.html',{'form':form})
+
