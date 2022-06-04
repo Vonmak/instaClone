5e1178063f07478a1e5878f72d5730a4b8e15ca0
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-04 17:40:53 +0300

    created a login form.

--- clone/forms.py
+++ clone/forms.py
@@ -6,3 +6,8 @@ class customRegistrationForm( UserCreationForm, forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name','last_name','email','username','password1','password2')
+        
+class LoginForm(forms.Form):
+    email=forms.CharField(max_length=50)
+    password=forms.CharField(max_length=20, widget=forms.PasswordInput)
+  
