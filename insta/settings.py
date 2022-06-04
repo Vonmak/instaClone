f0aedbad17112aec7b729db9aa889aa363c51394
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-04 17:43:04 +0300

    added an authentication backend.

--- insta/settings.py
+++ insta/settings.py
@@ -143,3 +143,9 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+AUTHENTICATION_BACKENDS=[
+    'django.contrib.auth.backends.ModelBackend',
+    'clone.customAuth.EmailAuth'
+]
