c7fdf78ddd70bd63b3c904d16527c701ada1f63e
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-04 20:36:02 +0300

    added a path for the index and profiles.

--- clone/urls.py
+++ clone/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
     path('register/', views.customUserRegister),
-     path('login/',views.login_user),
+    path('login/',views.login_user),
+    path('',views.index, name='index'),
+    re_path(r'^profile/(\d+)', views.profile, name='profile'),
 ]
