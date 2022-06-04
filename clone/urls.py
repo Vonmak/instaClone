ec79bc925cbff22c3ffc3141ff8163ac209ca686
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-04 17:42:18 +0300

    added a login path to the urls.

--- clone/urls.py
+++ clone/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.customUserRegister)
+    path('register/', views.customUserRegister),
+     path('login/',views.login_user),
 ]
