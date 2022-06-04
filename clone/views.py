5db3e2208c7abeaa5d406068223097c1c1b662e7
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-04 09:37:45 +0300

    addeda configuration for sending emails.

--- clone/views.py
+++ clone/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import customRegistrationForm
+from django.contrib.auth.models import User
+from .emails import send_welcome_email
 
 # Create your views here.
 def customUserRegister(request):
@@ -8,6 +10,11 @@ def customUserRegister(request):
     if request.method == 'POST':
         form = customRegistrationForm(request.POST)
         if form.is_valid():
+            # form.save()
+            first_name = form.cleaned_data['first_name']
+            email = form.cleaned_data['email']
+            recipient = User(first_name=first_name, email=email)
+            send_welcome_email(first_name,email)
             form.save()
         return HttpResponse('Successfully registered')
     return render(request, 'auth/register.html',{'form': form})
