0fc1b31a09477e7420208d5cd3c73461d5c14d51
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-05 14:00:06 +0300

    upadted the models.

--- clone/models.py
+++ clone/models.py
@@ -4,12 +4,15 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Location(models.Model):
-    name = models.CharField(max_length=40)
+class Profile(models.Model):
+    photo = CloudinaryField('image')
+    bio = models.CharField(max_length=40)
+    owner = models.OneToOneField(User,blank=True, on_delete=models.CASCADE, related_name="profile")
+    
     
     @classmethod
     def get_all(cls):
-        loc = Location.objects.all()
+        loc = Profile.objects.all()
         return loc
     
     def __str__(self):
@@ -19,11 +22,13 @@ class Location(models.Model):
     
 class Image(models.Model):
     image = CloudinaryField('image')
-    imageOwner = models.ForeignKey(User, on_delete=models.DO_NOTHING,)
+    user = models.ForeignKey(User,blank=True, on_delete=models.CASCADE)
     imageName= models.CharField(max_length=70)
-    imageDescription= models.TextField()
-    imageLocation= models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    imageCaption= models.TextField()
+    profile= models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     pub_date= models.DateTimeField(auto_now_add=True)
+    likes= models.IntegerField(default=0)
+    dislikes= models.IntegerField(default=0)
     
     
     def __str__(self):
@@ -35,5 +40,42 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()
     
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
     
+
+
+# class Preference(models.Model):
+#     user= models.ForeignKey(User, on_delete=models.CASCADE)
+#     post= models.ForeignKey(Image, on_delete=models.DO_NOTHING)
+#     value= models.IntegerField()
+#     date= models.DateTimeField(auto_now_add = True)
+
     
+#     def __str__(self):
+#         return str(self.user) + ':' + str(self.post) +':' + str(self.value)
+
+#     class Meta:
+#        unique_together = ("user", "post", "value")
+       
+       
+# class Comment(models.Model):
+#     image = models.ForeignKey(Image,blank=True, on_delete=models.CASCADE,related_name='comment')
+#     comment_owner = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+#     comment= models.TextField()
+
+#     def save_comment(self):
+#         self.save()
+
+#     def delete_comment(self):
+#         self.delete()
+
+#     @classmethod
+#     def get_image_comments(cls, id):
+#         comments = Comment.objects.filter(image__pk=id)
+#         return comments
+
+#     def __str__(self):
+#         return str(self.comment)
