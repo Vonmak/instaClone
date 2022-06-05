b7f21e553cafcc0614cda374d6e5d756220ff668
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-05 14:01:25 +0300

    added urls for logout and imagerequest.

--- clone/urls.py
+++ clone/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns=[
     path('register/', views.customUserRegister),
     path('login/',views.login_user),
+    path('logout',views.logout_user),
     path('',views.index, name='index'),
     re_path(r'^profile/(\d+)', views.profile, name='profile'),
+    path('image/', views.image_request, name = "image"),
+   
 ]
